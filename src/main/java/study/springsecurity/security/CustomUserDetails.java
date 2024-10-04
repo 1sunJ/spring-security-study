a63b7217664ef128b0b96d8c66da233125d4e4a0
haus <won9619v@naver.com>
2024-10-04 16:01:21 +0900

    Feat: CustomUserDetails 추가

---
+++ src/main/java/study/springsecurity/security/CustomUserDetails.java
@@ -0,0 +1,35 @@
+package study.springsecurity.security;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private final String email;
+    private final String password;
+
+    public CustomUserDetails(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+}
