23dc9d3a72d19576303a0d7f030c964d5a4d3646
haus <won9619v@naver.com>
2024-10-04 22:05:28 +0900

    Test: JwtAuthenticationFilter 로그 추가

--- src/main/java/study/springsecurity/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/study/springsecurity/security/filter/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,6 +20,7 @@ import study.springsecurity.security.manager.JwtManager;
 import java.io.IOException;
 import java.util.NoSuchElementException;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -29,18 +31,22 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("========== JwtFilter start ==========");
 
         String token = jwtManager.extractToken(request);
+        log.info("token : {}", token);
 
         if (jwtManager.validateToken(token)) {
             String email = jwtManager.getEmail(token);
             Member member = memberRepository.findByEmail(email).orElseThrow(NoSuchElementException::new);
+            log.info("email : {}", email);
 
             UserDetails userDetails = new CustomUserDetails(member);
             Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, "", jwtManager.getAuthorities(token));
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
 
+        log.info("========== JwtFilter end ==========");
         filterChain.doFilter(request, response);
     }
 
