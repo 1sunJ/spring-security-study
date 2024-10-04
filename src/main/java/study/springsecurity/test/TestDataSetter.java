2d0a5fc58f879f83478dc82096d50510baf08c00
haus <won9619v@naver.com>
2024-10-04 22:04:01 +0900

    Fix: test data Member 누락된 authority 추가

--- src/main/java/study/springsecurity/test/TestDataSetter.java
+++ src/main/java/study/springsecurity/test/TestDataSetter.java
@@ -3,6 +3,7 @@ package study.springsecurity.test;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import study.springsecurity.member.Member;
@@ -18,7 +19,9 @@ public class TestDataSetter {
     @EventListener(ApplicationReadyEvent.class)
     public void initTestData() {
         Member member1 = new Member("1sunJ", "won9619v@naver.com", "1234");
+        member1.addAuthority(new SimpleGrantedAuthority("ROLE_NORMAL"));
         Member member2 = new Member("2hunA", "abcdefg@naver.com", "1234");
+        member2.addAuthority(new SimpleGrantedAuthority("ROLE_NORMAL"));
         memberRepository.save(member1);
         memberRepository.save(member2);
     }
