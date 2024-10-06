641d673cb90ffd60d361add0a9bbeace5c0f4539
1sunJ <won9619v@naver.com>
2024-10-06 15:48:17 +0900

    Feat: JwtAuthenticationFilter doFilter 로직 수정

--- src/main/java/study/springsecurity/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/study/springsecurity/security/filter/JwtAuthenticationFilter.java
@@ -6,19 +6,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import study.springsecurity.member.Member;
 import study.springsecurity.member.MemberRepository;
-import study.springsecurity.security.domain.CustomUserDetails;
+import study.springsecurity.security.domain.JwtAuthentication;
 import study.springsecurity.security.manager.JwtManager;
 
 import java.io.IOException;
-import java.util.NoSuchElementException;
 
 @Slf4j
 @Component
@@ -26,8 +21,8 @@ import java.util.NoSuchElementException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final MemberRepository memberRepository;
-
     private final JwtManager jwtManager;
+    private final AuthenticationManager authenticationManager;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -36,24 +31,41 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         String token = jwtManager.extractToken(request);
         log.info("token : {}", token);
 
-        if (token != null) {
-            jwtManager.validateToken(token);
-
-            String email = jwtManager.getEmail(token);
-            Member member = memberRepository.findByEmail(email).orElseThrow(NoSuchElementException::new);
-            log.info("email : {}", email);
-
-            UserDetails userDetails = new CustomUserDetails(member);
-            Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, "", jwtManager.getAuthorities(token));
-            authentication.setAuthenticated(true);
-
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        } else {
-            log.info("no token request");
-        }
+        JwtAuthentication jwtAuthentication = new JwtAuthentication(token);
+        authenticationManager.authenticate(jwtAuthentication);
 
         log.info("========== JwtFilter end ==========");
         filterChain.doFilter(request, response);
     }
+
+    /**
+     * 기존 로직
+     */
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+//        log.info("========== JwtFilter start ==========");
+//
+//        String token = jwtManager.extractToken(request);
+//        log.info("token : {}", token);
+//
+//        if (token != null) {
+//            jwtManager.validateToken(token);
+//
+//            String email = jwtManager.getEmail(token);
+//            Member member = memberRepository.findByEmail(email).orElseThrow(NoSuchElementException::new);
+//            log.info("email : {}", email);
+//
+//            UserDetails userDetails = new CustomUserDetails(member);
+//            Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, "", jwtManager.getAuthorities(token));
+//            authentication.setAuthenticated(true);
+//
+//            SecurityContextHolder.getContext().setAuthentication(authentication);
+//        } else {
+//            log.info("no token request");
+//        }
+//
+//        log.info("========== JwtFilter end ==========");
+//        filterChain.doFilter(request, response);
+//    }
 
 }
